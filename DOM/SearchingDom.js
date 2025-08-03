7b4527f5a4e66cf258a431a865a5b71613239e3e
ramakrishnareddy9 <ramakrishnareddy.d9@gmail.com>
2025-08-03 19:53:42 +0530

    modifying TableNavagation, adding SearchingDom

---
+++ DOM/SearchingDom.js
@@ -0,0 +1,17 @@
+// change card title to red
+
+// by class for 0th element in array
+// let cardTitile = document.getElementsByClassName("card-title")[0]
+// cardTitile.style.color = "red"
+// console.log(cardTitile)
+
+// by id
+// let cardTitile = document.getElementById("firstcardtitle");
+// cardTitile.style.color = "red"
+// console.log(cardTitile)
+
+let cardTitile = document.querySelectorAll(".card-title")
+cardTitile[0].style.color = "red"
+cardTitile[1].style.color = "blue"
+cardTitile[2].style.color = "orange"
+console.log(cardTitile)
